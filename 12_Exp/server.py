c3fa25ffe45492be3545815e9b0cde139d37c265
Ayyyman <53811550+Ayyyman@users.noreply.github.com>
2020-12-31 10:01:45 +0530

    Update server.py

--- 12_Exp/server.py
+++ 12_Exp/server.py
@@ -1,14 +1,15 @@
 from socket import *
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(("LAPTOP-0KUL59J7", serverPort))
-print ("The server is ready to receive")
+serverSocket.bind(("127.0.0.1", serverPort))
+print("The server is ready to receive")
 while 1:
-     sentence,clientAddress = serverSocket.recvfrom(2048)
+    sentence, clientAddress = serverSocket.recvfrom(2048)
 
-     file=open(sentence,"r")
-     l=file.read(2048)
+    file = open(sentence, "r")
+    l = file.read(2048)
 
-     serverSocket.sendto(bytes(l,"utf-8"),clientAddress)
-     print("sent back to client",l)
-file.close()
+    serverSocket.sendto(bytes(l, "utf-8"), clientAddress)
+    print("Sent back to client: ", l)
+    file.close()
